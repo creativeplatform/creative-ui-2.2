9f372b55292ec6b858c32a3992cee42d94fb240a
Alexander Greene <greenealexander036@gmail.com>
2022-12-04 00:23:50 -0600

    fix: add loading spinners

--- src/components/common/Navbar/SignIn.tsx
+++ src/components/common/Navbar/SignIn.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useState } from "react";
-// @ts-ignore
-import { PrivateKey } from '@textile/hub'
-import { BigNumber, providers, utils } from 'ethers'
-import bcryptjs from 'bcryptjs';
+import React, { useState } from 'react'
+import { providers } from 'ethers'
 import {
   Modal,
   ModalOverlay,
@@ -12,13 +9,10 @@ import {
   useDisclosure,
   Button,
   Flex,
-  Box,
   Heading,
   FormControl,
   FormLabel,
-  FormErrorMessage,
   FormHelperText,
-  Select,
   Input,
   InputGroup,
   InputRightElement,
@@ -26,70 +20,67 @@ import {
   Stack,
   useToast,
   useColorModeValue,
-} from "@chakra-ui/react";
-import { TextileInstance } from "../../../services/textile/textile";
-import SignUp from './SignUp';
-import LogoModal from './LogoModal';
-import { useEthers } from "@usedapp/core";
-import { useAuth } from "../../../services/context/auth";
-import { UserModel } from "../../../services/textile/types";
+} from '@chakra-ui/react'
+import LogoModal from './LogoModal'
+import { useAuth } from '../../../services/context/auth'
 
-type WindowInstanceWithEthereum = Window & typeof globalThis & { ethereum?: providers.ExternalProvider };
+type WindowInstanceWithEthereum = Window &
+  typeof globalThis & { ethereum?: providers.ExternalProvider }
 class StrongType<Definition, Type> {
   // @ts-ignore
-  private _type: Definition;
+  private _type: Definition
   constructor(public value?: Type) {}
 }
 export class EthereumAddress extends StrongType<'ethereum_address', string> {}
 
 const SignIn = (props) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [closeButtons , setCloseButtons] = useState(false)
-  const [secret, setSecret] = useState<string>();
-  
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [closeButtons, setCloseButtons] = useState(false)
+  const [secret, setSecret] = useState<string>()
+  const [isLoggingIn, setIsLoggingIn] = useState(false)
+
   const toast = useToast()
 
-  const { login, getIdentity } = useAuth();
+  const { login, getIdentity } = useAuth()
 
-  const handleChange = (e: any) => setSecret(e.target.value);
+  const handleChange = (e: any) => setSecret(e.target.value)
 
-  const handleLogin = async (): Promise<void> => {
-    const identity = await getIdentity(secret);
-    console.log("LOGIN: ", identity)
-    
-    await login(identity);
+  const handleLogin = async () => {
+    setIsLoggingIn(true)
+    const identity = await getIdentity(secret)
+    console.log('LOGIN: ', identity)
+
+    await login(identity)
 
     createNotification(identity.public.toString())
     setCloseButtons(true)
     onClose()
-
-    return;
   }
 
   const createNotification = (identity?: string) => {
-    toast({ 
-      title: "Secret Key",
-      status: "success",
+    toast({
+      title: 'Secret Key',
+      status: 'success',
       description: ` SIGNED IN! Public Key: ${identity} Your app can now generate and reuse this users PrivateKey for creating user Mailboxes, Threads, and Buckets.`,
       duration: 9000,
       isClosable: true,
-    });
+    })
   }
 
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
-  const handleClick = () => setShow(!show);
+  const handleClick = () => setShow(!show)
 
   return (
     <>
-      <Button variant="ghost" size="md" onClick={onOpen} >
+      <Button variant="ghost" size="md" onClick={onOpen}>
         Sign In
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose} size="xl" isCentered>
         <ModalOverlay />
         <ModalContent borderRadius="10px" h="400px">
-          <ModalCloseButton />
+          <ModalCloseButton color="white" />
           <ModalBody
             borderRadius="10px"
             bgGradient="linear(to-r, rgb(40, 92, 163), rgb(229, 1, 105))"
@@ -97,31 +88,55 @@ const SignIn = (props) => {
           >
             <Flex alignItems="center" pt="2%" justifyContent="space-between">
               <Stack spacing={1}>
-              <LogoModal />
-              <Heading fontSize="2rem" color={useColorModeValue("white", "white")}>CREATIVE</Heading>
+                <LogoModal />
+                <Heading
+                  fontSize="2rem"
+                  color={useColorModeValue('white', 'white')}
+                >
+                  CREATIVE
+                </Heading>
               </Stack>
               <Container>
-              <Stack spacing={6}>
-                <Heading as="h6" size="md" color={useColorModeValue("white", "white")}>Sign In</Heading>
+                <Stack spacing={6}>
+                  <Heading
+                    as="h6"
+                    size="md"
+                    color={useColorModeValue('white', 'white')}
+                  >
+                    Sign In
+                  </Heading>
                   <FormControl id="login" isRequired>
-                  <FormLabel color={useColorModeValue("white", "white")}>Password</FormLabel>
+                    <FormLabel color={useColorModeValue('white', 'white')}>
+                      Password
+                    </FormLabel>
                     <InputGroup>
                       <Input
                         name="password"
-                        color={"white"}
+                        color={'white'}
                         placeholder="Password"
-                        type={show ? "text" : "password"}
-                        
+                        type={show ? 'text' : 'password'}
                         onChange={handleChange}
                       />
                       <InputRightElement width="4.5rem">
-                      <Button h="1.75rem" size="sm" onClick={handleClick} color={useColorModeValue("gray.900", "white")}>
-                        {show ? "Hide" : "Show"}
-                      </Button>
-                    </InputRightElement>
+                        <Button
+                          h="1.75rem"
+                          size="sm"
+                          onClick={handleClick}
+                          color={useColorModeValue('gray.900', 'white')}
+                        >
+                          {show ? 'Hide' : 'Show'}
+                        </Button>
+                      </InputRightElement>
                     </InputGroup>
                     <FormHelperText>enter account password</FormHelperText>
-                    <Button onClick={handleLogin} padding={2} color={useColorModeValue("gray.900", "white")}>Login with Metamask</Button>
+                    <Button
+                      isLoading={isLoggingIn}
+                      onClick={handleLogin}
+                      padding={2}
+                      color={useColorModeValue('gray.900', 'white')}
+                    >
+                      Login with Metamask
+                    </Button>
                   </FormControl>
                 </Stack>
               </Container>
@@ -130,7 +145,7 @@ const SignIn = (props) => {
         </ModalContent>
       </Modal>
     </>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
